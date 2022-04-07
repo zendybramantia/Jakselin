1f3a88be7825944d4c15a819e3272c8eadf28c88
zendybramantia <zendyoke@gmail.com>
2022-04-07 12:30:20 +0700

    view rifki

---
+++ resources/views/editKuliner.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=`, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light shadow-sm p-3 mb-5 bg-body rounded">
+        <a class="navbar-brand" href="#">
+            <img class="ms-sm-3" src="/images/Vector.svg" alt="" height="24">
+        </a>    
+        <div class="d-flex justify-content-between" style="width: 30%;">
+            <form class="d-flex">
+                <input class="form-control me-2 rounded-pill" type="search" placeholder="Cari Tempat Kuliner..." aria-label="Search">
+            </form>
+            <a class="navbar-brand" href="#">Home</a>
+            <a href="">
+                <img style="height: 36px;" src="/images/Ellipse 11.svg" alt="">
+            </a>
+        </div>
+    </nav>
+    
+    <div>
+        <h2 class="judul">UBAH REKOMENDASI KULINER</h2>
+    </div>
+    
+    <div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Nama Tempat</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Alamat</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Deskripsi</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        <div class="mb-3">
+            <label for="formFile" class="form-label">Gambar</label>
+            <input class="form-control" type="file" id="formFile">
+        </div>
+    </div>
+    
+    <div class="button">
+        <a href="" class="btn btn-outline-secondary">SUBMIT</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>   
+</body>
+</html>
