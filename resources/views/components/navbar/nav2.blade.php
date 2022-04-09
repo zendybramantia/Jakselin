0f38fc28395076e08f18b0391225ec3befd7e044
azhar <azhar03456789@gmail.com>
2022-04-09 16:21:04 +0700

    update component nav2

---
+++ resources/views/components/navbar/nav2.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-light bg-light shadow-sm p-3 mb-5 bg-body rounded">
+    <a class="navbar-brand" href="#">
+        <img class="ms-sm-3" src="/images/Vector.svg" alt="" height="24">
+    </a>    
+    <div class="d-flex justify-content-between" style="width: 30%;">
+        <form class="d-flex">
+            <input class="form-control me-2 rounded-pill" type="search" placeholder="Cari Tempat Kuliner..." aria-label="Search">
+        </form>
+        <a class="navbar-brand" href="#">Home</a>
+        <a href="">
+            <img style="height: 36px;" src="/images/Ellipse 11.svg" alt="">
+        </a>
+    </div>
+</nav>
--- resources/views/editKuliner.blade.php
+++ resources/views/editKuliner.blade.php
@@ -13,21 +13,7 @@
 
 </head>
 <body>
-    <nav class="navbar navbar-light bg-light shadow-sm p-3 mb-5 bg-body rounded">
-        <a class="navbar-brand" href="#">
-            <img class="ms-sm-3" src="/images/Vector.svg" alt="" height="24">
-        </a>    
-        <div class="d-flex justify-content-between" style="width: 30%;">
-            <form class="d-flex">
-                <input class="form-control me-2 rounded-pill" type="search" placeholder="Cari Tempat Kuliner..." aria-label="Search">
-            </form>
-            <a class="navbar-brand" href="#">Home</a>
-            <a href="">
-                <img style="height: 36px;" src="/images/Ellipse 11.svg" alt="">
-            </a>
-        </div>
-    </nav>
-    
+    <x-navbar.nav2/>
     <div>
         <h2 class="judul">UBAH REKOMENDASI KULINER</h2>
     </div>
