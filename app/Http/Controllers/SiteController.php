2d91fc319221f4b117a2b3d5d50dd9307ee40a4d
azhar <azhar03456789@gmail.com>
2022-04-09 15:58:41 +0700

    update

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class SiteController extends Controller {
-    public function index(){
-        return view('login');
+    public function index(Request $request){
+        $Username = $request->session()->get("email-login");
+        $Password = $request->session()->get("pass-login");
+        return view('login', [$Username, $Password]);
     }
 }
