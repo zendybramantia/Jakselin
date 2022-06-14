e5fda262672d89c7ffc49f8bbded13cd18f4c010
azhar <azhar03456789@gmail.com>
2022-06-15 00:58:42 +0700

    debug can laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,14 +26,14 @@
           <div class="d-flex justify-content-end" style="width: 16%;">
             @auth
               @if (app()->view->getSections()['title'] === "Home" || app()->view->getSections()['title'] === "Category")
-              @can('admin', \App\Http\Middleware\isAdmin::class)
+                @can('admin', \App\Http\Middleware\isAdmin::class)
+                @endcan
                 <a class="navbar-brand btn btn-warning" href="/dashboard/home"><small>ADMINISTRATOR</small></a>
-              @endcan
 
-              <a class="navbar-brand" href="/User/profile">{{ auth()->user()->name }}</a>
-              <a href="/User/profile">
-                  <img style="height: 36px;width: 36px; object-fit: cover;" class="rounded-circle" src="/{{ auth()->user()->avatar }}" alt="">
-              </a>
+                <a class="navbar-brand" href="/User/profile">{{ auth()->user()->name }}</a>
+                <a href="/User/profile">
+                    <img style="height: 36px;width: 36px; object-fit: cover;" class="rounded-circle" src="/{{ auth()->user()->avatar }}" alt="">
+                </a>
               @endif
             @else
                 <a class="navbar-brand" href="/login">Login</a>
